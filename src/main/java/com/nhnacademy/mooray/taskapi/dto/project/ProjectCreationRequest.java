2f87a13b3abbbb6e089e5f31478dfc5e89f46625
cororing <corock.dev@gmail.com>
2022-06-11 23:03:15 +0900

    커밋 빼먹은 DTO 추가

---
+++ src/main/java/com/nhnacademy/mooray/taskapi/dto/project/ProjectCreationRequest.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.mooray.taskapi.dto.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class ProjectCreationRequest {
+
+    @NotNull
+    private final Long adminId;
+
+    @NotBlank
+    private final String name;
+
+    @NotBlank
+    private final String description;
+
+    @NotNull
+    private final String status;
+
+}
