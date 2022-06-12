6bbf776de1ca3260b50f7798e6caa5e8a4dcdd41
cororing <corock.dev@gmail.com>
2022-06-12 15:46:48 +0900

    [프로젝트] 컨트롤러 슬라이싱 테스트 코드 작성 1차 완료

--- src/main/java/com/nhnacademy/mooray/taskapi/dto/project/ProjectUpdateRequest.java
+++ src/main/java/com/nhnacademy/mooray/taskapi/dto/project/ProjectUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.mooray.taskapi.dto.project;
 
+import com.nhnacademy.mooray.taskapi.entity.ProjectStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,4 +25,12 @@ public class ProjectUpdateRequest {
     @NotNull
     private final String status;
 
+    public static ProjectUpdateRequest sample() {
+        return ProjectUpdateRequest.builder()
+                                   .adminId(1L)
+                                   .name("2022-03-김해-웹서비스개발-1기")
+                                   .description("프로젝트 설명글이 있습니다.")
+                                   .status(ProjectStatus.ARCHIVED.name())
+                                   .build();
+    }
 }
--- src/test/java/com/nhnacademy/mooray/taskapi/controller/ProjectRestControllerTest.java
+++ src/test/java/com/nhnacademy/mooray/taskapi/controller/ProjectRestControllerTest.java
@@ -3,20 +3,21 @@ package com.nhnacademy.mooray.taskapi.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.mooray.taskapi.dto.MoorayResult;
 import com.nhnacademy.mooray.taskapi.dto.project.ProjectCreationRequest;
+import com.nhnacademy.mooray.taskapi.dto.project.ProjectUpdateRequest;
 import com.nhnacademy.mooray.taskapi.service.project.ProjectService;
 import org.junit.jupiter.api.*;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,8 +38,6 @@ class ProjectRestControllerTest {
     @DisplayName("성공적으로 프로젝트 생성")
     @Test
     void createProject() throws Exception {
-        ProjectCreationRequest projectRequest = ProjectCreationRequest.sample();
-
         /**
          * EXCEPTION:
          *
@@ -53,44 +52,39 @@ class ProjectRestControllerTest {
          * @Builder
          * Add **@Getter**
          */
-        String requestBody = objectMapper.writeValueAsString(projectRequest);
-
         BDDMockito.given(projectService.createProject(any(ProjectCreationRequest.class)))
                   .willReturn(mock(MoorayResult.class));
 
+        String requestBody = objectMapper.writeValueAsString(ProjectCreationRequest.sample());
         this.mockMvc.perform(post("/projects")
                                      .contentType(APPLICATION_JSON)
                                      .content(requestBody))
                     .andDo(print())
                     .andExpect(status().isCreated())
                     .andExpect(content().contentType(APPLICATION_JSON));
-                    // .andExpect(jsonPath("$.isSuccess", equalTo(true)));
+        // .andExpect(jsonPath("$.isSuccess", equalTo(true)));
     }
 
-    // @Order(2)
-    // @DisplayName("성공적으로 프로젝트 정보 수정")
-    // @Test
-    // void updateProject() throws Exception {
-    //     Mockito.when(projectService.updateProject(any(ProjectUpdateRequest.class)))
-    //            .thenReturn(MoorayResult.success(anyString(), anyMap()));
-    //
-    //     String projectName = "2022-03-김해-웹서비스개발-1기";
-    //     ProjectUpdateRequest projectRequest = ProjectUpdateRequest.builder()
-    //                                                               .adminId(1L)
-    //                                                               .name(projectName)
-    //                                                               .description("프로젝트 설명글이 있습니다.")
-    //                                                               .status(ProjectStatus.ARCHIVED.name())
-    //                                                               .build();
-    //
-    //     String requestBody = objectMapper.writeValueAsString(projectRequest);
-    //
-    //     this.mockMvc.perform(put("/projects")
-    //                                  .contentType(APPLICATION_JSON)
-    //                                  .content(requestBody))
-    //                 .andDo(print())
-    //                 .andExpect(status().isOk())
-    //                 .andExpect(content().contentType(APPLICATION_JSON))
-    //                 .andExpect(jsonPath("$.isSuccess", equalTo(true)));
-    // }
+    @Order(2)
+    @DisplayName("성공적으로 프로젝트 정보 수정")
+    @Test
+    void updateProject() throws Exception {
+        // given
+        BDDMockito.given(projectService.updateProject(any(ProjectUpdateRequest.class)))
+                  .willReturn(mock(MoorayResult.class));
+
+        // ready
+        String requestBody = objectMapper.writeValueAsString(ProjectUpdateRequest.sample());
+
+        // when
+        this.mockMvc.perform(put("/projects")
+                                     .contentType(APPLICATION_JSON)
+                                     .content(requestBody))
+                    // then
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(APPLICATION_JSON));
+                    // .andExpect(jsonPath("$.isSuccess", equalTo(true)));
+    }
 
 }
