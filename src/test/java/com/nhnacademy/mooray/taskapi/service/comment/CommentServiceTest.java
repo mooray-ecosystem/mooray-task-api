edac4bafe99af8ed745561bcb05a36c5de86c987
cororing <corock.dev@gmail.com>
2022-06-13 00:36:45 +0900

    [댓글] 서비스 레이어 테스트 기본 와꾸 추가

---
+++ src/test/java/com/nhnacademy/mooray/taskapi/service/comment/CommentServiceTest.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.mooray.taskapi.service.comment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+
+    @DisplayName("")
+    @Test
+    void testCreateComment() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testRetrieveComments() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testRetrieveComment() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testUpdateComment() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testDeleteComment() {
+    }
+
+}
