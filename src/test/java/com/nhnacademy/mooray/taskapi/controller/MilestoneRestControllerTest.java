7ec2904507eeb621f4eac98031a0f6d412eed129
cororing <corock.dev@gmail.com>
2022-06-13 03:46:04 +0900

    [마일스톤] 테스트 파일 와꾸 작성

---
+++ src/test/java/com/nhnacademy/mooray/taskapi/controller/MilestoneRestControllerTest.java
@@ -0,0 +1,35 @@
+package com.nhnacademy.mooray.taskapi.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = MilestoneRestController.class)
+class MilestoneRestControllerTest {
+
+    @DisplayName("")
+    @Test
+    void testCreateMilestone() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testRetrieveMilestones() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testRetrieveMilestone() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testUpdateTaskMilestone() {
+    }
+
+    @DisplayName("")
+    @Test
+    void testDeleteMilestone() {
+    }
+
+}
