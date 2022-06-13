c079a2ff7b158012db25f146efbfd2b17c46b732
cororing <corock.dev@gmail.com>
2022-06-14 00:14:02 +0900

    [댓글] 깨지는 코드 보완

--- src/main/java/com/nhnacademy/mooray/taskapi/dto/tag/TagUpdateRequest.java
+++ src/main/java/com/nhnacademy/mooray/taskapi/dto/tag/TagUpdateRequest.java
@@ -2,9 +2,11 @@ package com.nhnacademy.mooray.taskapi.dto.tag;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class TagUpdateRequest {
