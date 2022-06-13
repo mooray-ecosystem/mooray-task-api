cb69bbb9caa7f2e484c0773d8ca63858f4336c8a
cororing <corock.dev@gmail.com>
2022-06-13 19:59:42 +0900

    [댓글] 컨트롤러 테스트 커버리지 100% 달성

--- src/main/java/com/nhnacademy/mooray/taskapi/controller/CommentRestController.java
+++ src/main/java/com/nhnacademy/mooray/taskapi/controller/CommentRestController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.mooray.taskapi.controller;
 import com.nhnacademy.mooray.taskapi.dto.MoorayResult;
 import com.nhnacademy.mooray.taskapi.dto.comment.CommentCreationRequest;
 import com.nhnacademy.mooray.taskapi.dto.comment.CommentUpdateRequest;
+import com.nhnacademy.mooray.taskapi.entity.Comment;
 import com.nhnacademy.mooray.taskapi.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,54 +25,54 @@ public class CommentRestController {
     private final CommentService commentService;
 
     @PostMapping("/projects/{project-id}/tasks/{task-id}/comments")
-    public ResponseEntity<Map<String, Object>> createComment(@PathVariable("project-id") Long projectId,
+    public ResponseEntity<MoorayResult<Comment>> createComment(@PathVariable("project-id") Long projectId,
                                                              @PathVariable("task-id") Long taskId,
                                                              @RequestBody CommentCreationRequest commentRequest) {
 
         // FIXME: Refactor logging
         log.error("c.n.mooray.taskapi.controller.CommentRestController: Enter createComment");
 
-        MoorayResult result = commentService.createComment(projectId, taskId, commentRequest);
+        MoorayResult<Comment> result = commentService.createComment(projectId, taskId, commentRequest);
 
         return ResponseEntity.status(CREATED)
                              .contentType(APPLICATION_JSON)
                              .headers(httpHeaders -> httpHeaders.setAccept(List.of(APPLICATION_JSON)))
-                             .body(result.getPayload());
+                             .body(result);
     }
 
     @GetMapping("/projects/{project-id}/tasks/{task-id}/comments")
-    public ResponseEntity<Map<String, Object>> retrieveComments(@PathVariable("project-id") Long projectId,
+    public ResponseEntity<MoorayResult<List<Comment>>> retrieveComments(@PathVariable("project-id") Long projectId,
                                                                 @PathVariable("task-id") Long taskId) {
 
         // FIXME: Refactor logging
         log.error("c.n.mooray.taskapi.controller.CommentRestController: Enter retrieveComments");
 
-        MoorayResult result = commentService.retrieveComments(projectId, taskId);
+        MoorayResult<List<Comment>> result = commentService.retrieveComments(projectId, taskId);
 
-        return ResponseEntity.status(CREATED)
+        return ResponseEntity.status(OK)
                              .contentType(APPLICATION_JSON)
                              .headers(httpHeaders -> httpHeaders.setAccept(List.of(APPLICATION_JSON)))
-                             .body(result.getPayload());
+                             .body(result);
     }
 
     @GetMapping("/projects/{project-id}/tasks/{task-id}/comments/{comment-id}")
-    public ResponseEntity<Map<String, Object>> retrieveComment(@PathVariable("project-id") Long projectId,
+    public ResponseEntity<MoorayResult<Comment>> retrieveComment(@PathVariable("project-id") Long projectId,
                                                                @PathVariable("task-id") Long taskId,
                                                                @PathVariable("comment-id") Long commentId) {
 
         // FIXME: Refactor logging
         log.error("c.n.mooray.taskapi.controller.CommentRestController: Enter retrieveComment");
 
-        MoorayResult result = commentService.retrieveComment(projectId, taskId, commentId);
+        MoorayResult<Comment> result = commentService.retrieveComment(projectId, taskId, commentId);
 
         return ResponseEntity.status(OK)
                              .contentType(APPLICATION_JSON)
                              .headers(httpHeaders -> httpHeaders.setAccept(List.of(APPLICATION_JSON)))
-                             .body(result.getPayload());
+                             .body(result);
     }
 
     @PutMapping("/projects/{project-id}/tasks/{task-id}/comments/{comment-id}")
-    public ResponseEntity<Map<String, Object>> updateComment(@PathVariable("project-id") Long projectId,
+    public ResponseEntity<MoorayResult<Comment>> updateComment(@PathVariable("project-id") Long projectId,
                                                              @PathVariable("task-id") Long taskId,
                                                              @PathVariable("comment-id") Long commentId,
                                                              @RequestBody CommentUpdateRequest commentRequest) {
@@ -79,28 +80,28 @@ public class CommentRestController {
         // FIXME: Refactor logging
         log.error("c.n.mooray.taskapi.controller.CommentRestController: Enter updateComment");
 
-        MoorayResult result = commentService.updateComment(projectId, taskId, commentId, commentRequest);
+        MoorayResult<Comment> result = commentService.updateComment(projectId, taskId, commentId, commentRequest);
 
         return ResponseEntity.status(OK)
                              .contentType(APPLICATION_JSON)
                              .headers(httpHeaders -> httpHeaders.setAccept(List.of(APPLICATION_JSON)))
-                             .body(result.getPayload());
+                             .body(result);
     }
 
     @DeleteMapping("/projects/{project-id}/tasks/{task-id}/comments/{comment-id}")
-    public ResponseEntity<Map<String, Object>> deleteComment(@PathVariable("project-id") Long projectId,
+    public ResponseEntity<MoorayResult<Boolean>> deleteComment(@PathVariable("project-id") Long projectId,
                                                              @PathVariable("task-id") Long taskId,
                                                              @PathVariable("comment-id") Long commentId) {
 
         // FIXME: Refactor logging
         log.error("c.n.mooray.taskapi.controller.CommentRestController: Enter deleteComment");
 
-        MoorayResult result = commentService.deleteComment(projectId, taskId, commentId);
+        MoorayResult<Boolean> result = commentService.deleteComment(projectId, taskId, commentId);
 
         return ResponseEntity.status(OK)
                              .contentType(APPLICATION_JSON)
                              .headers(httpHeaders -> httpHeaders.setAccept(List.of(APPLICATION_JSON)))
-                             .body(result.getPayload());
+                             .body(result);
     }
 
 }
